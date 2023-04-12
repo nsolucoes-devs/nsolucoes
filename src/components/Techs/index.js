347e3f244a1d587aad5e583f1f72b74925c7272b
vitor-nsolucoes <vitor@nsolucoes.digital>
2023-04-12 15:18:54 -0300

    app div

--- src/components/Techs/index.js
+++ src/components/Techs/index.js
@@ -1,5 +1,16 @@
 import React, { useRef, useState, useEffect } from "react";
-import { Container, ProductColumn, ProductRow, TechRow } from "./styles";
+import {
+  Container,
+  ProductColumn,
+  ProductRow,
+  TechRow,
+  MobileApp,
+  BackgroundMobile,
+  AppDetails,
+  Phone,
+  Logo,
+  InBuildBtn
+} from "./styles";
 import Title from "../Title";
 import { IoLogoReact } from "../../styles/Icons";
 import stack1 from "../../assets/images/stack1.png";
@@ -13,7 +24,9 @@ import stack8 from "../../assets/images/stack8.png";
 import stack9 from "../../assets/images/stack9.png";
 import ScrollContainer from "react-indiana-drag-scroll";
 import logzz from "../../assets/images/logzz.png";
-import mooncoded from "../../assets/images/mooncoded.png"
+import mooncoded from "../../assets/images/mooncoded.png";
+import phoneMockup from "../../assets/images/phone-mockup.png";
+import logzzLogo from "../../assets/images/logzz-logo.png";
 
 function Techs() {
   const stacks = {
@@ -60,25 +73,17 @@ function Techs() {
           </p>
           <strong>Ferramentas de desenvolvimento</strong>
           <ul>
-            <li>
-                + HTML5
-            </li>
-            <li>
-                + PHP
-            </li>
-            <li>
-                + CSS3
-            </li>
-            <li>
-                + MYSQL
-            </li>
+            <li>+ HTML5</li>
+            <li>+ PHP</li>
+            <li>+ CSS3</li>
+            <li>+ MYSQL</li>
           </ul>
         </ProductColumn>
         <img className="laptop" src={logzz} />
       </ProductRow>
       <div className="divider" />
       <ProductRow>
-      <img className="laptop" src={mooncoded} />
+        <img className="laptop" src={mooncoded} />
         <ProductColumn>
           <h1>mooncoded</h1>
           <p>
@@ -89,21 +94,34 @@ function Techs() {
           </p>
           <strong>Ferramentas de desenvolvimento</strong>
           <ul>
-            <li>
-                + REACTJS
-            </li>
-            <li>
-                + Styled-Components
-            </li>
-            <li>
-                + MongoSQL
-            </li>
-            <li>
-                + Github API
-            </li>
+            <li>+ REACTJS</li>
+            <li>+ Styled-Components</li>
+            <li>+ MongoSQL</li>
+            <li>+ Github API</li>
           </ul>
         </ProductColumn>
       </ProductRow>
+      <div className="divider" />
+      <MobileApp>
+        <div className="main-wrapper">
+          <AppDetails>
+            <div className="app-title">
+              <Logo src={logzzLogo} /> <h1>Logzz App</h1>
+            </div>
+            <h2>O aplicativo que conecta clientes e entregadores.</h2>
+            <h4>A praticidade chegou pra quem precisa de segurança e confiança!</h4>
+
+            <InBuildBtn>
+            🔨 Em construção
+            </InBuildBtn>
+          </AppDetails>
+          <div className="right-side">
+            <div className="phone-holder">
+              <Phone src={phoneMockup} />
+            </div>
+          </div>
+        </div>
+      </MobileApp>
     </Container>
   );
 }
--- src/components/Techs/styles.js
+++ src/components/Techs/styles.js
@@ -14,7 +14,7 @@ export const Container = styled.div`
   .divider {
     width: 90%;
     height: 2px;
-    background-color: rgba(0,0,0,0.03);
+    background-color: rgba(0, 0, 0, 0.03);
     margin: 20px 0;
   }
 
@@ -32,54 +32,192 @@ export const Container = styled.div`
 `;
 
 export const ProductRow = styled.div`
-    width: 100%;
-    display: flex;
-    flex-direction: row;
-    align-items: center;
-    gap: 20px;
-    padding: 26px;
-    justify-content: center;
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  gap: 20px;
+  padding: 26px;
+  justify-content: center;
 
-    @media (max-width: 1100px) {
-        flex-direction: column-reverse;
-
-        .laptop {
-            max-width: 100% !important;
-        }
-    }
+  @media (max-width: 1100px) {
+    flex-direction: column-reverse;
 
     .laptop {
-        max-width: 40%;
-        cursor: pointer;
+      max-width: 100% !important;
     }
+  }
+
+  .laptop {
+    max-width: 40%;
+    cursor: pointer;
+  }
 `;
 
 export const ProductColumn = styled.div`
-    width: 25%;
+  width: 25%;
+  display: flex;
+  flex-direction: column;
+  gap: 15px;
+
+  @media (max-width: 1100px) {
+    width: 80%;
+    text-align: center;
+  }
+
+  h1 {
+    text-transform: uppercase;
+  }
+
+  strong {
+    text-transform: uppercase;
+  }
+
+  ul {
+    list-style: none;
+    margin-left: 20px;
+
+    li {
+      margin: 10px 0;
+    }
+  }
+`;
+
+export const MobileApp = styled.div`
+  width: 70%;
+  min-height: 400px;
+  border-radius: 20px;
+  margin-top: 100px;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  padding: 26px;
+  background-color: #27272e;
+  border-radius: 20px;
+  position: relative;
+  -webkit-clip-path: inset(-100vw 0 0 -100vw);
+  clip-path: inset(-100vw 0 0 -100vw);
+
+  .main-wrapper {
     display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .right-side {
+    position: relative;
+    width: 100%;
+    padding-left: 12px;
+    padding-right: 12px;
+    flex: 0 0 50%;
+    max-width: 50%;
+  }
+
+  .phone-holder {
+    position: absolute;
+    width: 130%;
+    max-width: 130%;
+    top: -16rem;
+  }
+
+  @media (max-width: 900px) {
     flex-direction: column;
+    width: 90%;
+
+    .main-wrapper {
+      flex-direction: column;
+    }
+
+    .right-side {
+      max-width: 100%;
+    }
+
+    .phone-holder {
+      position: relative;
+      width: 100%;
+      max-width: 100%;
+      top: 0px;
+      margin-top: 50px;
+
+      img {
+        position: relative;
+        width: 100%;
+      }
+    }
+  }
+`;
+
+export const Phone = styled.img`
+  transition: all 0.2s;
+  width: 100%;
+  max-width: 100%;
+  cursor: pointer;
+
+  :hover {
+    transform: scale(1.07);
+  }
+`;
+
+export const BackgroundMobile = styled.div`
+  position: absolute;
+  width: 100%;
+  bottom: 0;
+  background-color: blue;
+  height: 20%;
+  clip-path: inset(-100vmax -100vmax 50%); /* cut the bottom */
+`;
+
+export const AppDetails = styled.div`
+  display: flex;
+  flex-direction: column;
+  color: #fff;
+  gap: 20px;
+
+  width: 100%;
+  min-width: 70%;
+
+  .app-title {
+    display: flex;
+    flex-direction: row;
     gap: 15px;
+    align-items: center;
+    font-size: 1.5rem;
+  }
 
-    @media (max-width: 1100px) {
-        width: 80%;
-        text-align: center;
+  h2 {
+    font-size: 1.5rem;
+  }
+
+  h4 {
+    font-weight: 500;
+  }
+
+  @media (max-width: 1100px) {
+    min-width: 0% !important;
+
+    .app-title {
+      flex-direction: column;
     }
+  }
 
-    h1 {
-        text-transform: uppercase;
-    }
+  @media (max-width: 900px) {
+    text-align: center;
+    justify-content: center;
+    align-items: center;
+  }
+`;
 
-    strong {
-        text-transform: uppercase;
-    }
+export const Logo = styled.img`
+  width: 80px;
+  height: 80px;
+  border-radius: 20px;
+`;
 
-    ul {
-        list-style: none;
-        margin-left: 20px;
-
-        li {
-            margin: 10px 0;
-        }
-    }
-
+export const InBuildBtn = styled.div`
+  padding: 12px 25px;
+  background-color: #565663;
+  border-radius: 20px;
+  width: fit-content;
+  cursor: pointer;
 `;
