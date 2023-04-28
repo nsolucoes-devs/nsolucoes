076a76eede08db8fd21d7bb848cce81d1e584275
joice-nsolucoes <joice@nsolucoes.digital>
2023-04-28 14:07:21 -0300

    adjusting cards about row

--- src/components/Card/styles.js
+++ src/components/Card/styles.js
@@ -8,6 +8,17 @@ export const Wrapper = styled.div`
   margin: 25px;
   position: relative;
   cursor: pointer;
+
+  @media screen and (min-width: 360px) and (max-width: 760px){
+    max-width: 300px;
+    min-width: 300px;
+  }
+
+  @media only screen and (max-width: 768px) {
+    max-width: 350px;
+    min-width: 350px;
+  }
+
 `;
 
 export const MainContainer = styled.div`
