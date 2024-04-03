57a6b1dc6d6b77b2a983df079fa9d8554e2472dd
Alan Cristian Silva <alan@nsolucoes.digital>
2024-04-03 16:22:28 -0300

    Update index.js

--- src/pages/About/index.js
+++ src/pages/About/index.js
@@ -8,7 +8,7 @@ import { RiPieChartFill } from "react-icons/ri";
 import { IoSpeedometer } from "react-icons/io5";
 import { IoTime } from "react-icons/io5";
 import { FaGear } from "react-icons/fa6";
-import AppVideo from "../../assets/videos/ecommerce.mp4";
+import AppVideo from "../../assets/videos/app.mp4";
 
 function About() {
   const cardIcon = [
